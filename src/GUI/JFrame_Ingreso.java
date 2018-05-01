560a416a2da0430f21b8912cb9cad254f2fbde82
Yael Chavoya <realidadvirtualwm@gmail.com>
2018-05-01 14:04:43 -0500

    Revert "error"
    This reverts commit 8930945a52c0b25b9b32314e2b59f25012b285ce.

--- src/GUI/JFrame_Ingreso.java
+++ src/GUI/JFrame_Ingreso.java
@@ -42,12 +42,7 @@ public class JFrame_Ingreso extends JFrame implements ActionListener{
         /*Ventana*/
         super("Ingreso"); 
         this.setResizable(false);
-        try{
-            setIconImage(new ImageIcon(getClass().getResource("../Imagen/icono.png")).getImage());
-        }
-        catch(Exception e){
-            
-        }
+        setIconImage(new ImageIcon(getClass().getResource("../Imagen/icono.png")).getImage());
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(500,250);
         loadFrameDetails();
