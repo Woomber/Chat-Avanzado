13f4739a95af7d5ce3163062d88da3f7451f5112
Tofiter <christopheradan9@gmail.com>
2018-04-30 23:06:32 -0500

    HiloTransmisor

--- src/Threads/Thread_Transmitter.java
+++ src/Threads/Thread_Transmitter.java
@@ -5,15 +5,45 @@
  */
 package Threads;
 
+import PaquetesModels.Paquete;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import Json.JsonParser;
+
 /**
  *
  * @author PC
  */
-public class Thread_Transmitter implements Runnable{
+public class Thread_Transmitter implements Runnable {
+
+    private String user;
+    private String pass;
+
+    public Thread_Transmitter() {
+    }
+
+    public Thread_Transmitter(String user, String pass) {
+        this.user = user;
+        this.pass = pass;
+    }
 
     @Override
     public void run() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            Socket socketTx = new Socket("localhost", 90);
+            PrintWriter pw = new PrintWriter(socketTx.getOutputStream(), true);
+            BufferedReader read = new BufferedReader(new InputStreamReader(socketTx.getInputStream()));
+            while (true) {
+
+            }
+
+        } catch (IOException e) {
+            e.getMessage();
+        }
+
     }
-    
+
 }
